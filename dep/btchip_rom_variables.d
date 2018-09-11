f67e95451981bbcff963b932cb55cac6fbc1d544
barbariska108 <boris11001@gmail.com>
2018-09-11 13:07:34 +0000

    build

---
+++ dep/btchip_rom_variables.d
@@ -0,0 +1,30 @@
+obj/btchip_rom_variables.o: src/btchip_internal.h \
+  include/btchip.h include/btchip_config.h \
+  /opt/bolos/nanos-secure-sdk/include/os.h \
+  /opt/bolos/nanos-secure-sdk/include/bolos_target.h \
+  /opt/bolos/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stddef.h \
+  /opt/bolos/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stdint.h \
+  /usr/include/stdint.h /usr/include/features.h \
+  /usr/include/stdc-predef.h /usr/include/sys/cdefs.h \
+  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+  /usr/include/gnu/stubs-32.h /usr/include/bits/wchar.h \
+  /opt/bolos/nanos-secure-sdk/include/os_apilevel.h \
+  /opt/bolos/nanos-secure-sdk/include/cx.h \
+  /opt/bolos/nanos-secure-sdk/include/cx_compliance_141.h \
+  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+  /usr/include/bits/waitstatus.h /usr/include/endian.h \
+  /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+  /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+  /usr/include/bits/byteswap-16.h /usr/include/sys/types.h \
+  /usr/include/time.h /usr/include/sys/select.h \
+  /usr/include/bits/select.h /usr/include/bits/sigset.h \
+  /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
+  /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
+  /usr/include/bits/stdlib-float.h \
+  /opt/bolos/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stdbool.h \
+  include/btchip_secure_value.h src/btchip_public_ram_variables.h \
+  include/btchip_context.h include/btchip_filesystem_tx.h \
+  src/btchip_public_ram_key_variables.h src/btchip_rom_variables.h \
+  include/btchip_filesystem.h include/btchip_base58.h \
+  include/btchip_bcd.h include/btchip_ecc.h include/btchip_helpers.h \
+  src/btchip_transaction.h include/btchip_apdu_constants.h
